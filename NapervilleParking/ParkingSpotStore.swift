b5366a6ffa8f08424b55a8668d18d028f72e991b
Jon Allured <jon.allured@gmail.com>
2015-07-05 02:17:22 -0500

    Fetch Central count from server

--- NapervilleParking/ParkingSpotStore.swift
+++ NapervilleParking/ParkingSpotStore.swift
@@ -8,11 +8,17 @@ protocol ParkingSpotStoreDelegate {
 
 class ParkingSpotStore {
     var delegate: ParkingSpotStoreDelegate?
+    var jeffersonUpperCount = ""
+    var chicagoMiddleCount = ""
+    var jeffersonLowerCount = ""
     var vanBurenCount = ""
     
     func update() {
-        let url = "http://www.naperville.il.us/parkingfacilities/vanburen.aspx"
-        Alamofire.request(.GET, url).responseString(encoding: nil, completionHandler: parseVanBurenResponse)
+        let vanBurenUrl = "http://www.naperville.il.us/parkingfacilities/vanburen.aspx"
+        Alamofire.request(.GET, vanBurenUrl).responseString(encoding: nil, completionHandler: parseVanBurenResponse)
+        
+        let centralUrl = "http://www.naperville.il.us/parkingfacilities/central.aspx"
+        Alamofire.request(.GET, centralUrl).responseString(encoding: nil, completionHandler: parseCentralResponse)
     }
     
     private func parseVanBurenResponse(request: NSURLRequest, response: NSHTTPURLResponse?, string: String?, error: NSError?) {
@@ -24,6 +30,36 @@ class ParkingSpotStore {
                 
                 if let count = xml["span"].element?.text {
                     self.vanBurenCount = count
+                }
+                
+                self.delegate?.didUpdateCounts(self)
+        }
+    }
+    
+    private func parseCentralResponse(request: NSURLRequest, response: NSHTTPURLResponse?, string: String?, error: NSError?) {
+        if let string = string,
+            let openRange = string.rangeOfString("<table"),
+            let closeRange = string.rangeOfString("</table>") {
+                var table = string[openRange.startIndex..<closeRange.endIndex]
+                
+                while table.rangeOfString("&nbsp;") != nil {
+                    if let range = table.rangeOfString("&nbsp;") {
+                        table.removeRange(range)
+                    }
+                }
+                
+                let xml = SWXMLHash.parse(table)
+                
+                if let count = xml["table"]["tr"][1]["td"][1]["p"]["span"].element?.text {
+                    self.jeffersonUpperCount = count
+                }
+                
+                if let count = xml["table"]["tr"][3]["td"][1]["p"]["span"].element?.text {
+                    self.chicagoMiddleCount = count
+                }
+                
+                if let count = xml["table"]["tr"][5]["td"][1]["p"]["span"].element?.text {
+                    self.jeffersonLowerCount = count
                 }
                 
                 self.delegate?.didUpdateCounts(self)
--- NapervilleParking/ViewController.swift
+++ NapervilleParking/ViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 class ViewController: UIViewController {
     let parkingSpotStore = ParkingSpotStore()
     
+    @IBOutlet weak var jeffersonUpperLabel: UILabel!
+    @IBOutlet weak var chicagoMiddleLabel: UILabel!
+    @IBOutlet weak var jeffersonLowerLabel: UILabel!
     @IBOutlet weak var vanBurenLabel: UILabel!
 
     override func viewDidLoad() {
@@ -16,5 +19,8 @@ class ViewController: UIViewController {
 extension ViewController: ParkingSpotStoreDelegate {
     func didUpdateCounts(store: ParkingSpotStore) {
         self.vanBurenLabel.text = store.vanBurenCount
+        self.jeffersonUpperLabel.text = store.jeffersonUpperCount
+        self.chicagoMiddleLabel.text = store.chicagoMiddleCount
+        self.jeffersonLowerLabel.text = store.jeffersonLowerCount
     }
 }
